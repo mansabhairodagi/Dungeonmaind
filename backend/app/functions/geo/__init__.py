023bd08513be3789b430fedb9989d281b50436ea
malavika <malavikaks1997@gmail.com>
2026-08-27 17:33:31 +0200

    Export resolve_map_locations

--- backend/app/functions/geo/__init__.py
+++ backend/app/functions/geo/__init__.py
@@ -1,1 +1,13 @@
 """Geographic place resolution utilities."""
+
+from app.functions.geo.place_resolver import (
+    normalize_place_name,
+    resolve_location_entities,
+    resolve_map_locations,
+)
+
+__all__ = [
+    'normalize_place_name',
+    'resolve_location_entities',
+    'resolve_map_locations',
+]
